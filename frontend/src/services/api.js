138433f02751dbd2e9a7b3c2d65b979f6915d4aa
João Pedro Moura <joaomoura.269@gmail.com>
2026-02-01 16:08:59 -0300

    Alinhamento parcial com prod

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
     "X-API-KEY": import.meta.env.VITE_API_KEY,
   },
 });
